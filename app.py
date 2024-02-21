7e8f01b09ccf51d4a42575a261a6332353aae90f
xuandat7 <xuandattran2003@example.com>
2024-02-21 16:13:39 +0700

    test

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import smtplib
 from email.mime.text import MIMEText
 app=Flask(__name__)
 app.config["SECRET_KEY"]="quanhoangduong"
-server = 'LAPTOP-FF387IJ3\HOANGQUAN'
-database = 'Account'
+server = 'XuanDat'
+database = 'dbweb'
 username = 'quan'
 password = '123456'
 connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};Trusted_connection=yes;'
